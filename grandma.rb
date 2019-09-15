b8ae7ff07fdcbba267d7a54b44985103e0d9968e
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-15 03:43:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
   return "NO, NOT SINCE 1938!"
   
  elsif phrase == "I LOVE YOU GRANDMA!"
- puts "I LOVE YOU TOO PUMPKIN!"
+ return "I LOVE YOU TOO PUMPKIN!"
 
   end
 end 
